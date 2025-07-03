c97d6b01eb9356bcf9636e512ca0db5a817b3c13
mauroslucios <mauroslucios@gmail.com>
2025-07-03 20:41:10 -0300

    usando streamlit

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(layout="wide" if True else "centered" , page_title="Hello, Streamlit!", page_icon=":wave:")
+st.title("Hello, Streamlit!")
+
+st.write("This is a simple Streamlit app to demonstrate the layout configuration.")
